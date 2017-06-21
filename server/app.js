5f4824fee433b126fd53cada4479d64d293ef777
Julian Craigwell-Graham <julian@Julians-MacBook-Pro.local>
2017-06-21 15:59:47 -0500

    first commit

--- server/app.js
+++ server/app.js
@@ -1,15 +1,42 @@
 var express = require('express'),
-    app     = express(),
-    server  = require('http').createServer(app);
+   app     = express(),
+    server  = require('http').createServer(app),
+    path = require('path');
+
+    var movie ={'title': 'he got game', 'year': 1999}
+    var Lakers ={'city': 'LA', 'conference': 'west'}
+
+//tell express that our views can be found in ./views
+app.set('views', path.join(__dirname, 'views'));
+//tell express that we are using handlebars as our view engin
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
-  app.get('/', function(request, response){
+  app.get('/text', function(request, response){
     // request object is from the client,
     // the response is what were sending back
     response.send('Hey I the server finally works')
   })
+  app.get('/home', function(req, res){
+
+	res.render('home');
+})
+  app.get('/profile', function(req, res){
+
+	res.render('profile');
+})
+
+
+ app.get('/movie', function(req, res){
+
+	res.json(movie);
+})
+
+ app.get('/lakers', function(req, res){
+
+	res.json(Lakers);
+})
 
 
 
